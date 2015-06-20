1be26947d57267c26cb8f29ee9f01c69f9719904
Dražen Lučanin <kermit666@gmail.com>
2015-06-21 00:19:10 +0200

    accept http as well

--- pagekite.sh
+++ pagekite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pagekite --clean --frontend=$CLOUDFLEET_HOST \
-         --service_on=https:blimp.$CLOUDFLEET_DOMAIN:nginx:443:$CLOUDFLEET_SECRET \
+         --service_on=http,https:blimp.$CLOUDFLEET_DOMAIN:nginx:443:$CLOUDFLEET_SECRET \
          --logfile=/opt/cloudfleet/data/logs/pagekite/pagekite.log
